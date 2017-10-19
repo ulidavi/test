fd8928a335981e69c1a745ce746e0e20f6656d42
David Li <david.li@ville.montreal.qc.ca>
2017-10-19 12:46:01 -0400

    Master 2

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Test2 master");
+            Console.WriteLine("Test2 master 2");
         }
     }
 }
